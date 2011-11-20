fb7e3104a41aa66a45c5211d272f93a5ef242c45
regenerationweb <regenerationweb@ea9fe2a2-87ed-4418-96c7-9d50d3de4e2c>
2011-11-20 12:54:28 +0000

    JS updates.
    git-svn-id: http://svn.g9labs.com/regenerationweb/trunk@68 ea9fe2a2-87ed-4418-96c7-9d50d3de4e2c

--- sites/all/themes/regenerationweb/js/regenerationweb.js
+++ sites/all/themes/regenerationweb/js/regenerationweb.js
@@ -2,12 +2,6 @@ var THEME_COLORS = ['#E0401C', '#E5B051', '#272F30', '#F7EDB7', '#9E2B20'];
 
 (function($) {
   $(window).ready(function() {
-    // Lazy-load google maps script:
-    var script = document.createElement("script");
-    script.type = "text/javascript";
-    script.src = "http://maps.googleapis.com/maps/api/js?sensor=false&callback=regenGMapInit";
-    document.body.appendChild(script);
-
     // Image carousel widget on front page.
     $('.flex-slider').flexslider({
           animation: "fade",
@@ -30,26 +24,19 @@ var THEME_COLORS = ['#E0401C', '#E5B051', '#272F30', '#F7EDB7', '#9E2B20'];
       }
     };
     renderTopEdgeColors($('.edge-colors'));
+
+    // Convert MP3 files to native players on the podcast page.
+    $('#aggregator h3.feed-item-title [href$=mp3]').each(function(idx, el) {
+      var containerEl = $(el).closest('.feed-item');
+      var linkEl = $('<a></a>').attr("href", el.href).text('Download');
+      var downloadEl = linkEl.clone().addClass('media-embed').text($(el).text());
+      containerEl.find('.feed-item-meta').after(linkEl).after(downloadEl);
+    });
+    
+    $('.media-embed').media({
+      height: 24,
+      caption: false,
+      bgColor: "transparent"
+    });
   })
 })(jQuery);
-
-// The callback on script load will load this fn, which renders the Google Map
-// into the div.
-function regenGMapInit() {
-  // Regen addr: 238 E 15th st Oakland
-  var lat = 37.798388;
-  var lng = -122.254935;
-  var regen = new google.maps.LatLng(lat, lng);
-  var myOptions = {
-    zoom: 14,
-    center: regen,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  }
-  var map = new google.maps.Map(document.getElementById("regen-gmaps"), myOptions);
-  var marker = new google.maps.Marker({
-    map:map,
-    draggable:false,
-    animation: google.maps.Animation.DROP,
-    position: regen
-  });
-}
