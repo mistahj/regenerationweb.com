653abf3c9f921bc7b1400a2046c976a590435809
blurb user <blurbuser@MAC292.local>
2014-09-08 01:32:48 -0700

    Make it mobile responsive.

--- sites/all/themes/regenerationweb/templates/html.tpl.php
+++ sites/all/themes/regenerationweb/templates/html.tpl.php
@@ -59,11 +59,12 @@
  * @see zen_preprocess_html()
  * @see template_process()
  */
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
-  "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
+?>
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
 
 <head profile="<?php print $grddl_profile; ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
